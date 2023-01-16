0fdf9e72887c12274b04495ba5f103387ff92316
Jason Robinson <json.robinson@gmail.com>
2023-01-16 08:49:56 -0500

    Completed Largest Prime exercise

--- LargestPrimeChallenge/src/Main.java
+++ LargestPrimeChallenge/src/Main.java
@@ -1,60 +1,70 @@
+
 /**
  * Coding exercise 26: Largest Prime
- * 
+ *
  * @author jason
  */
 public class Main {
 
+    /**
+     * Calculate the largest prime factor of a given number.
+     * 
+     * Non-destructive method.
+     * (Solution taken from geeksforgeeks.org)
+     * 
+     * @param number
+     * @return The largest prime factor
+     */
     public static int getLargestPrime(int number) {
 
-        if (number < 2) { return -1; }
-        
-        if(number == 2 || number == 3){ return number; }
-        
-        int maxPrime = -1;
-        if(number < 40){
-        for (int i = 1; i <= number; i++) {
-            if ((6 * i + 1) == number || (6 * i - 1) == number) {
-                maxPrime = i;
-            }
-        }
-        } else {
-            for (int i = 0; i < number; i++) {
-                maxPrime = i * i + i + 41;
-            }
+        if (number < 2) {
+            return -1;
         }
         
-        return maxPrime;
+        int n = number;
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        
+        for (int i = 3; i < Math.sqrt(n); i += 2) {
+            while(n % i == 0){
+                n /= i;
+            }
+        }
+        
+        return n;
     }
-   
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         testGetLargestPrime(
-                new int[]{21, 217, 0, 45, -1}, 
+                new int[]{21, 217, 0, 45, -1},
                 new int[]{7, 31, -1, 5, -1});
     }
-    
-    private static void testGetLargestPrime(int[] inputs, int[] outputs){
-        
+
+    private static void testGetLargestPrime(int[] inputs, int[] outputs) {
+
         int output;
-        
+
         int maxAnswers = outputs.length + 1;
         int maxInputs = inputs.length;
         int n = 0;
-        
-        do{
+
+        do {
             output = getLargestPrime(inputs[n]);
-            
-            System.out.print((output == outputs[n]) ? "PASSED " 
+
+            System.out.print((output == outputs[n]) ? "PASSED "
                     : "FAILED, expected: " + outputs[n] + "\n");
-            System.out.println("getLargestPrime( " + inputs[n] + " ) => " + output); 
-            
+            System.out.println("getLargestPrime( " + inputs[n] + " ) => " + output);
+
             n++;
-            if(n >= maxInputs){ break; }
-            
-        } while(n < maxAnswers);
-        
+            if (n >= maxInputs) {
+                break;
+            }
+
+        } while (n < maxAnswers);
+
     }
 }
