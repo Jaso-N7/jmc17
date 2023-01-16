66392fdf82777be7c1c5b05be34f5b9b9dfbb606
Jason Robinson <json.robinson@gmail.com>
2023-01-16 18:57:55 -0500

    WIP: Initial solution

---
+++ DiagonalStarExercise/src/Main.java
@@ -0,0 +1,44 @@
+/**
+ *
+ * @author jason
+ */
+public class Main {
+
+    /**
+     * Prints diagonals to generate a rectangular pattern composed of stars (*).
+     * 
+     * @param number 
+     */
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        }
+        
+        for (int rows = 0; rows <= number; rows++) {
+            for (int cols = 0; cols <= number; cols++) {
+                // In the first or last row
+                if(rows == 0 || rows == number ||
+                    // In the first or last column
+                    cols == 0 || cols == number ||
+                    // When the row number equals the column number
+                    rows == cols ||
+                    // When the column number equals rowCount - currentRow + 1
+                    cols == number - rows + 1){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    } // printSquareStar :: Integer -> IO String
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Print Square Star of size 8:");
+        printSquareStar(8);
+    }
+    
+}
