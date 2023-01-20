e5aed0f501c837e3ecbec49c31975c3e7935167c
Jason Robinson <json.robinson@gmail.com>
2023-01-20 11:29:15 -0500

    Initial project setup

---
+++ InputCalculator/src/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Udemy Course Java Master Class (JDK 17)
+ * Coding exercise 28: Input Calculator
+ */
+
+/**
+ *
+ * @author jason
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+    
+    /**
+     * Calculates the sum and average of numbers input by user
+     */
+    public static void inputThenPrintSumAndAverage(){
+        System.out.println("Not yet implemented");
+    } // inputThenPrintSumAndAverage :: IO String
+}
