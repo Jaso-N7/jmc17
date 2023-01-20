288543a0157d7b6d52e832606c24ce2d1d3b67b3
Jason Robinson <json.robinson@gmail.com>
2023-01-20 09:36:52 -0500

    Initial setup for the MinAndMax project

---
+++ MinAndMaxChallenge/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Min and Max Challenge using an endless loop
+ * 
+ * @author jason
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        collectMinMax();
+    }
+    
+    /**
+     * Prompts the user for numbers and keeps track of the minimum and maximum
+     * entered.
+     * 
+     * Minimum and maximum numbers will be continuously displayed.
+     */
+    public static void collectMinMax() {
+        System.out.println("Not yet implemented");
+    } // collectMinMax
+    
+    
+}
