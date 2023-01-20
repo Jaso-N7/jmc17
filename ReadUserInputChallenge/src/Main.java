d8c145e2a14a938e948a997ead87556f81934323
Jason Robinson <json.robinson@gmail.com>
2023-01-19 22:07:26 -0500

    Challenge completed

--- ReadUserInputChallenge/src/Main.java
+++ ReadUserInputChallenge/src/Main.java
@@ -1,6 +1,9 @@
+
+import java.util.Scanner;
+
 /**
  * Reading User Input Challenge
- * 
+ *
  * @author jason
  */
 public class Main {
@@ -9,17 +12,34 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        sumOfNumbers();
+        System.out.println(sumOfNumbers());
     } // psvm
-    
+
     /**
      * Read 5 valid numbers from the console, entered by the user, and print the
      * sum of those five numbers.
-     * @return 
+     *
+     * @return
      */
-    public static String sumOfNumbers(){
-        
-        int sum = 0;
+    public static String sumOfNumbers() {
+
+        int sum = 0,
+                counter = 1;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            
+            System.out.println("Enter number #" + counter);
+            try {
+                
+                sum += Integer.parseInt(scanner.nextLine());
+                counter++;
+                
+            } catch (NumberFormatException nfe) {
+                System.out.println("Invalid number");
+            }
+        } while (counter < 6);
+
         return "Total = " + sum;
     } // sumOfNumbers :: IO String
 }
