c25e8ff31c8fe3744a4ff70fa82450282cc407f9
Jason Robinson <json.robinson@gmail.com>
2023-01-19 21:59:05 -0500

    Initial setup for Reading User Input challenge

---
+++ ReadUserInputChallenge/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author jason
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        sumOfNumbers();
+    }
+    
+    public static String sumOfNumbers(){
+        
+        int sum = 0;
+        return "Total = " + sum;
+    }
+}
