c2f6905daf81277bf14fc0c7a743672f9fcf7b34
Jason Robinson <json.robinson@gmail.com>
2023-01-20 11:40:11 -0500

    Initial solution

--- InputCalculator/src/Main.java
+++ InputCalculator/src/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Udemy Course Java Master Class (JDK 17)
  * Coding exercise 28: Input Calculator
@@ -20,6 +23,28 @@ public class Main {
      * Calculates the sum and average of numbers input by user
      */
     public static void inputThenPrintSumAndAverage(){
-        System.out.println("Not yet implemented");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int sum = 0, inputCounter = 0;
+        long average = 0L;
+        String input;
+        
+        while(true){
+
+           input = scanner.nextLine(); 
+           ++inputCounter;
+            
+            try{
+                
+                sum += Integer.parseInt(input);
+                average = Math.round(sum / inputCounter);
+                
+            } catch (NumberFormatException nfe){
+                System.out.println("SUM = " + sum + " AVG = " + average);
+                break;
+            }
+        }
+        
     } // inputThenPrintSumAndAverage :: IO String
 }
