94c6d7f55e62a8bb3acba833036eff190acdb946
Jason Robinson <json.robinson@gmail.com>
2023-01-22 09:48:45 -0500

    Updated documentation and introduced another overloaded method

--- PaintJob/src/Main.java
+++ PaintJob/src/Main.java
@@ -17,13 +17,23 @@ public class Main {
         System.out.println("getBucketCount(2.75, 3.25, 2.5, 1) => (3) ? " +
                 getBucketCount(2.75, 3.25, 2.5, 1));
         
-        // Testing overloaded methods
-        System.out.println("\ngetBucketCount(-3.4, 2.1, 1.5) => (-1) ? " +
+        // Testing overloaded method without extra buckets
+        System.out.println("\nNo extra buckets at home:");
+        System.out.println("getBucketCount(-3.4, 2.1, 1.5) => (-1) ? " +
                 getBucketCount(-3.4, 2.1, 1.5));
         System.out.println("getBucketCount(3.4, 2.1, 1.5) => (5) ? " +
                 getBucketCount(3.4, 2.1, 1.5));
         System.out.println("getBucketCount(7.25, 4.3, 2.35) => (14) ? " +
                 getBucketCount(7.25, 4.3, 2.35));
+        
+        // Testing overloaded method without width and height
+        System.out.println("\nAlready knows the area of the wall:");
+        System.out.println("getBucketCount(3.4, 1.5) => (3) ? " +
+                getBucketCount(3.4, 1.5));
+        System.out.println("getBucketCount(6.26, 2.2) => (3) ? " + 
+                getBucketCount(6.26, 2.2));
+        System.out.println("getBucketCount(3.26, 0.75) => (5) ? " +
+                getBucketCount(3.26, 0.75));
     }
     
     /**
@@ -47,8 +57,31 @@ public class Main {
         return (int) Math.ceil((double) requiredBuckets - extraBuckets);
     }
     
+    /**
+     * Calculate how many buckets of paint needed to buy before going to work
+     * 
+     * @param width The width of the wall
+     * @param height The height of the wall
+     * @param areaPerBucket The area that can be covered with one bucket of paint
+     * @return The number of buckets needed to purchase before going to work.
+     */
     public static int getBucketCount(double width, double height, double areaPerBucket){
         return getBucketCount(width, height, areaPerBucket, 0);
     }
     
+    /**
+     * Calculate how many buckets of paint needed to buy before going to work
+     * 
+     * @param area The known area of the wall
+     * @param areaPerBucket The area that can be covered with one bucket of paint
+     * @return The number of buckets needed to buy before going to work.
+     */
+    public static int getBucketCount(double area, double areaPerBucket){
+        
+        if (area < 0 || areaPerBucket < 0) {
+            return -1;
+        }
+        
+        return 0;
+    }
 }
