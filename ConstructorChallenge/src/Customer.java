507cab50168fc32b9ba4741065dc6f8103c9a920
Jason Robinson <json.robinson@gmail.com>
2023-01-22 20:43:28 -0500

    Initial setup of the solution

---
+++ ConstructorChallenge/src/Customer.java
@@ -0,0 +1,39 @@
+/**
+ *
+ * @author jason
+ */
+public class Customer {
+    
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Customer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public Customer() {
+        this("J. Q. Public", 20_000d, "anon@no-reply.com");
+    }
+
+    public Customer(String name, String email) {
+        this(name, 20_000d, email);
+    }
+    
+    
+    
+}
