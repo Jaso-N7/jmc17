3041993450afa3da6eb2c39879e914091b360566
Jason Robinson <json.robinson@gmail.com>
2023-01-22 20:48:00 -0500

    No args constructor tested and working

--- ConstructorChallenge/src/Main.java
+++ ConstructorChallenge/src/Main.java
@@ -8,7 +8,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Customer customer = new Customer();
+        System.out.println("Name: " + customer.getName());
+        System.out.println("Email: "+ customer.getEmail());
+        System.out.println("Credit Limit: \u0024" + 
+                customer.getCreditLimit() + "\u00A2");
+        
+        
     }
     
 }
