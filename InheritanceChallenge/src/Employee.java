68027fafd741316c321a5591c33e19fc7a5e77a2
Jason Robinson <json.robinson@gmail.com>
2023-01-24 14:49:48 -0500

    Employee class created

---
+++ InheritanceChallenge/src/Employee.java
@@ -0,0 +1,37 @@
+/**
+ *
+ * @author jason
+ */
+public class Employee extends Worker {
+    
+    private long employeeId;
+    private String hireDate;
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Employee(long employeeId, String employeeName, 
+            String employeeDOB, String hireDate) {
+        
+        super(employeeName, employeeDOB);
+        this.employeeId = employeeId;
+        this.hireDate = hireDate;
+        
+    }
+    
+    
+    
+}
