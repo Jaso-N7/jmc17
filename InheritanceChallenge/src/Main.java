b08c95ed41878d8e7ffea6dade7ae6874ea585ec
Jason Robinson <json.robinson@gmail.com>
2023-01-24 22:52:06 -0500

    Able to retrieve info from SalariedEmployee class

--- InheritanceChallenge/src/Main.java
+++ InheritanceChallenge/src/Main.java
@@ -10,8 +10,16 @@ public class Main {
      */
     public static void main(String[] args) {
         
+        SalariedEmployee s = new SalariedEmployee(187_000, false, 101, "Yochannan",
+        "1976/05/18", "2019/05/01");
+        System.out.println(s.getName() + " is " + s.getAge() + " years old");
+        System.out.println(s.getName() + " collects " + s.collectPay());
+        System.out.println(s.getName() + " is " + 
+                ((s.isIsRetired()) ? " retired" : "still active in the field"));
+        System.out.println();
+        System.out.println(s);
         
-        
+        System.out.println();
     }
     
 }
