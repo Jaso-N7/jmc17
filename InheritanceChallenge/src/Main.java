98e51e42eac64810df67679ed846a514eea71a99
Jason Robinson <json.robinson@gmail.com>
2023-01-24 23:09:26 -0500

    Display information for HourlyEmployee and retired SalariedEmployee

--- InheritanceChallenge/src/Main.java
+++ InheritanceChallenge/src/Main.java
@@ -12,14 +12,26 @@ public class Main {
         
         SalariedEmployee s = new SalariedEmployee(187_000, false, 101, "Yochannan",
         "1976/05/18", "2019/05/01");
+        s.retire();
         System.out.println(s.getName() + " is " + s.getAge() + " years old");
         System.out.println(s.getName() + " collects " + s.collectPay());
         System.out.println(s.getName() + " is " + 
-                ((s.isIsRetired()) ? " retired" : "still active in the field"));
+                ((s.isIsRetired()) ? " retired on " + s.getEndDate() : "still active in the field"));
         System.out.println();
         System.out.println(s);
         
         System.out.println();
+        
+        HourlyEmployee h = new HourlyEmployee(45.00d, 102, "Shimeon ben Yaaqov",
+                "1985/12/09", "2013/09/25");
+        System.out.println(h.getName() + " started working as of " + h.getHireDate());
+        System.out.println("Being " + h.getAge() + " years old, he earns \u0024" +
+                h.getHourlyPayRate() + "\u00A2/hr.");
+        System.out.println("Due to his exception performance, he now earns $" +
+                h.getDoublePay() + "/hr");
+        System.out.println();
+        
+        System.out.println(h);
     }
     
 }
