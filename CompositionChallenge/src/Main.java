96e3c3595b7813f0c27898a5456dbc8057173170
Jason Robinson <json.robinson@gmail.com>
2023-01-30 12:36:39 -0500

    Execute the work needed to be done by the appliances

--- CompositionChallenge/src/Main.java
+++ CompositionChallenge/src/Main.java
@@ -8,7 +8,15 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Refrigerator r = new Refrigerator(true);
+        DishWasher d = new DishWasher(true);
+        CoffeeMaker c = new CoffeeMaker(true);
+        Oven o = new Oven(true);
+        
+        SmartKitchen kitchen = new SmartKitchen(o, d, r, c);
+        
+        kitchen.doKitchenWork();
     }
     
 }
