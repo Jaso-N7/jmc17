bac27a71859eb76364c6907a3701af1c62de221a
Jason Robinson <json.robinson@gmail.com>
2023-01-29 20:31:20 -0500

    Initial setup for Refrigerator

---
+++ CompositionChallenge/src/Refrigerator.java
@@ -0,0 +1,30 @@
+/**
+ * A Refrigerator is:
+ * 
+ * Refrigerator r = new Refrigerator(boolean);
+ * 
+ * INTERPRETATION: An IoT Appliance, in this case a refrigerator
+ * WHERE: It accepts one boolean argument, that determines if it has
+ * work to do.
+ * 
+ * @author jason
+ */
+public class Refrigerator {
+    
+    private boolean hasWorkToDo;
+    
+    public Refrigerator(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+    
+    public boolean isHasWorkToDo() {
+        return hasWorkToDo;
+    }
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+    
+    public void orderFood() {}
+
+}
