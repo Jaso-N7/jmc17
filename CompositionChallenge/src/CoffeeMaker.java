51e990e4dd44c9acf2841d765b13651434739823
Jason Robinson <json.robinson@gmail.com>
2023-01-29 20:35:35 -0500

    Initial setup for CoffeeMaker

---
+++ CompositionChallenge/src/CoffeeMaker.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author jason
+ */
+public class CoffeeMaker {
+    
+    private boolean hasWorkToDo;
+
+    public CoffeeMaker(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public boolean isHasWorkToDo() {
+        return hasWorkToDo;
+    }
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+    
+    public void brewCoffee() {}
+}
