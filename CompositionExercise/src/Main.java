efeb0cbef8fe53321de34333842bbfe9f8e7ea46
Jason Robinson <json.robinson@gmail.com>
2023-01-30 13:00:19 -0500

    Lamp class completed

---
+++ CompositionExercise/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Udemy Java Masterclass JDK 17 Coding Exercise 38: Composition
+ * 
+ * @author jason
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
