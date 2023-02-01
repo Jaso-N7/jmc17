ea9b20883ac8886269744b20c918c5c4383072c0
Jason Robinson <json.robinson@gmail.com>
2023-01-31 21:08:58 -0500

    Testing Duplex printer
    Some tests are failing when printing beyond the allotted toner level

--- EncapsulationChallenge/src/Main.java
+++ EncapsulationChallenge/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        // Setup a new non-duplex printer
+        // Setup a new non-duplex duplexPrinter
         Printer printer = new Printer();
         System.out.format("%s%n", printer.toString());
         printer.addToner(100);
@@ -26,7 +26,24 @@ public class Main {
         printer.printPages(85);
         System.out.format("%s%n", printer.toString());
         
-
+        System.out.println();
+        
+        // Setup a new duplex duplexPrinter
+        Printer duplexPrinter = new Printer(true);
+        System.out.format("%s%n", duplexPrinter.toString());
+        duplexPrinter.addToner(100);
+        System.out.println("Installed toner, current level " 
+                + duplexPrinter.getTonerLevel() + "%");
+        duplexPrinter.printPages(10);
+        System.out.format("%s%n", duplexPrinter.toString());
+        System.out.println("Attempting to install more 50% more toner");
+        System.out.println("addToner = " + duplexPrinter.addToner(50));
+        System.out.println("Printing 85 more pages");
+        duplexPrinter.printPages(85);
+        System.out.format("%s%n", duplexPrinter.toString());
+        System.out.println("Printing another 85 more pages");
+        duplexPrinter.printPages(85);
+        System.out.format("%s%n", duplexPrinter.toString());
         
     }
     
