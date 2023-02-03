ca06452cb1638bea46587d74b1f6ae1f1beb1c2a
Jason Robinson <json.robinson@gmail.com>
2023-02-03 15:44:25 -0500

    Modified based on Encapsulation Exercise

--- EncapsulationChallenge/src/Main.java
+++ EncapsulationChallenge/src/Main.java
@@ -9,20 +9,25 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        testPrinter(false);
-        
-        System.out.println();
-        
-        testPrinter(true);
-        
+//        testPrinter(false);
+//        
+//        System.out.println();
+//        
+//        testPrinter(true);
+//        
         // Encapsulation Exercise testing
         Printer printer = new Printer(50, true);
         System.out.println(printer.addToner(50));
-        System.out.println("initial page count = " +printer.getPagesPrinted());
+        System.out.println("initial page count = " + 
+                printer.getPagesPrinted());
         int pagesPrinted = printer.printPages(4);
-        System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
+        System.out.println("Pages printed was " + pagesPrinted + 
+                " new total print count for printer = " + 
+                printer.getPagesPrinted());
         pagesPrinted = printer.printPages(2);
-        System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
+        System.out.println("Pages printed was " + pagesPrinted + 
+                " new total print count for printer = " + 
+                printer.getPagesPrinted());
         
     }
     
--- EncapsulationChallenge/src/Printer.java
+++ EncapsulationChallenge/src/Printer.java
@@ -24,16 +24,28 @@ public class Printer {
      * NOTE: This function has the side-effect of reducing toner levels
      * 
      * @param pages 
+     * @return The amount of pages that have been printed
      */
-    public void printPages(int pages) {
+//    public void printPages(int pages) {
+//        
+//        
+//        // purpose of using the modulo: when there is an odd number of pages, 
+//        // an extra sheet will be included.
+//        pagesPrinted += duplex ? pages / 2 + pages % 2 : pages;
+//                
+//        // reduce toner level - side-effect?
+//        tonerLevel -= pagesPrinted;
+//    }
+    
+    public int printPages(int pages){
         
+        int jobPages = duplex ? pages / 2 + pages % 2 : pages;
+        if(duplex)
+            System.out.println("Printing in duplex mode");
         
-        // purpose of using the modulo: when there is an odd number of pages, 
-        // an extra sheet will be included.
-        pagesPrinted += duplex ? pages / 2 + pages % 2 : pages;
-                
-        // reduce toner level - side-effect?
-        tonerLevel -= pagesPrinted;
+        pagesPrinted += jobPages;
+        
+        return jobPages;
     }
 
     public Printer(int tonerLevel, boolean duplex) {
