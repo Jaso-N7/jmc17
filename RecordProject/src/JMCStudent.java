78a28394363aa48831c3e9c9e8e91681d0ea89fa
jason <jason@fulguros>
2023-02-12 12:45:40 -0500

    Java's implicit POJO Type and the Record Type

---
+++ RecordProject/src/JMCStudent.java
@@ -0,0 +1,8 @@
+/**
+ * From my understanding, this is similar to a macro for POJOs.
+ * Introduced in JDK 14, became standard in JDK 16
+ * @author jason
+ */
+public record JMCStudent(String id, String name, String dateOfBirth, String classList) {
+
+}
---
+++ RecordProject/src/Main.java
@@ -0,0 +1,67 @@
+
+import javax.swing.JOptionPane;
+
+/**
+ * An example of POJOs and Records
+ * @author jason
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+//        generateStudentPOJO();
+        generateStudentRecord();
+        
+        Student pojo = new Student("POJO1", "Freddie", "1999/02/05", "Java Masterclass");
+        JMCStudent record = new JMCStudent("REC01", "Ginger", "1994/10/05", "Java Masterclass");
+        
+        JOptionPane.showMessageDialog(null, 
+                String.format("%s%n".repeat(2), pojo , record));
+        
+        JOptionPane.showMessageDialog(null, String.format(
+                "%s is taking %s%n".repeat(2),
+                pojo.getName(), pojo.getClassList(),
+                record.name(), record.classList()));
+        
+    }
+
+    private static void generateStudentPOJO() {
+        for (int i = 1; i <= 5; i++) {
+            Student s = new Student("JMC0" + i, 
+                    switch(i){
+                        case 1 -> "Adrian";
+                        case 2 -> "Barkely";
+                        case 3 -> "Cameron";
+                        case 4 -> "Dave";
+                        case 5 -> "Eddie";
+                        default -> "Unregistered";
+                    },
+                    "198" + (i + 1) + '/' + (i * 2) + '/' + (i +3) , 
+                    "Java Master Class (JDK 17)");
+            
+            System.out.println(s);
+        }
+    }
+    
+    private static void generateStudentRecord() {
+        for (int i = 1; i <= 5; i++) {
+            JMCStudent s = new JMCStudent("JMC0" + i, 
+                    switch(i){
+                        case 1 -> "Adrian";
+                        case 2 -> "Barkely";
+                        case 3 -> "Cameron";
+                        case 4 -> "Dave";
+                        case 5 -> "Eddie";
+                        default -> "Unregistered";
+                    },
+                    "198" + (i + 1) + '/' + (i * 2) + '/' + (i +3) , 
+                    "Java Master Class (JDK 17)");
+            
+            System.out.println(s);
+        }
+    }
+    
+}
