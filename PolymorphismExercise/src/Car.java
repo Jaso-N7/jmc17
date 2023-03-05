051e58afde4120113116d12380febba15f11072e
jason <jason@fulguros>
2023-03-05 17:15:17 -0500

    First attempt at Coding Exercise 40

---
+++ PolymorphismExercise/src/Car.java
@@ -0,0 +1,89 @@
+public class Car {
+
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car (int cylinders, String name) {
+	this.cylinders = cylinders;
+	this.name = name;
+    }
+
+    public void startEngine () {
+	System.out.println("Car -> startEngine()");
+    }
+
+    public void accelerate () {
+	System.out.println("Car -> accelerate()");
+    }
+
+    public void brake () {
+	System.out.println("Car -> brake()");
+    }
+
+    public int getCylinders () {
+	return cylinders;
+    }
+
+    public String getName () {
+	return name;
+    }
+} // Car
+
+class Mistubishi extends Car {
+
+    public Mistubishi (int cylinders, String name) {
+	super(cylinders, name);
+    }
+
+    public void startEngine () {
+	System.out.println("Mitsubishi -> startEngine()");
+    }
+
+    public void accelerate () {
+	System.out.println("Mitsubishi -> accelerate()");
+    }
+
+    public void brake () {
+	System.out.println("Mitsubishi -> brake()");
+    }
+} // Mistubishi
+
+class Holden extends Car {
+
+    public Holden (int cylinders, String name) {
+	super(cylinders, name);
+    }
+
+    public void startEngine () {
+	System.out.println("Holden -> startEngine()");
+    }
+
+    public void accelerate () {
+	System.out.println("Holden -> accelerate()");
+    }
+
+    public void brake () {
+	System.out.println("Holden -> brake()");
+    }
+} // Holden
+
+class Ford extends Car {
+
+    public Ford (int cylinders, String name) {
+	super(cylinders, name);
+    }
+
+    public void startEngine () {
+	System.out.println("Ford -> startEngine()");
+    }
+
+    public void accelerate () {
+	System.out.println("Ford -> accelerate()");
+    }
+
+    public void brake () {
+	System.out.println("Ford -> brake()");
+    }
+} // Ford
