10c59a98007b9098566ac3b3a650d4d913717b02
jason <jason@fulguros>
2023-03-04 20:06:12 -0500

    Setup Car's constructor, getter and toString methods

--- PolymorphismChallenge/src/Car.java
+++ PolymorphismChallenge/src/Car.java
@@ -4,18 +4,33 @@
  */
 public class Car {
     private String description;
+
+    public Car(String description){
+	this.description = description;
+    }
+
+    public String getDescription() {
+	return description;
+    }
     
     public void startEngine () {
     }
     
-    public void drive () {}
+    public void drive () {
+	runEngine();
+    }
     
     protected void runEngine () {}
+
+    public String toString () {
+	return description;
+    }
 }
 
 class GasPoweredCar extends Car {
     private double avgKmPerLitre;
     private int cylinders;
+
 }
 
 class ElectricCar extends Car {
