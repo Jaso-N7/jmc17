a2e92ee4918f5afe34f950fd5bdc1b911bec898d
jason <jason@fulguros>
2023-03-13 14:13:42 -0500

    Implemented Constructor

--- BillsBurgersExercise/src/DeluxeBurger.java
+++ BillsBurgersExercise/src/DeluxeBurger.java
@@ -1,1 +1,12 @@
-public class DeluxeBurger {}
+public class DeluxeBurger extends Hamburger {
+
+    public DeluxeBurger () {
+	super("Deluxe", "Beef & Eggs", "White roll", 14.54);
+	addition1Name = "Chips";
+	addition1Price = 2.75d;
+	addition2Name = "Drink";
+	addition2Price = 1.81d;
+    }
+
+
+}
