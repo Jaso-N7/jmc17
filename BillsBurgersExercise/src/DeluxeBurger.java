954660d91bec23cfaff9971e68e053335ea78c65
jason <jason@fulguros>
2023-03-13 14:15:36 -0500

    Using the super methods to add the additional items within the constructor

--- BillsBurgersExercise/src/DeluxeBurger.java
+++ BillsBurgersExercise/src/DeluxeBurger.java
@@ -2,10 +2,8 @@ public class DeluxeBurger extends Hamburger {
 
     public DeluxeBurger () {
 	super("Deluxe", "Beef & Eggs", "White roll", 14.54);
-	addition1Name = "Chips";
-	addition1Price = 2.75d;
-	addition2Name = "Drink";
-	addition2Price = 1.81d;
+	super.addHamburgerAddition1("Chips", 2.75d);
+	super.addHamburgerAddition2("Drink", 1.81d);
     }
 
     @Override
