37f319567fa230683349dafe990bd7645794b229
jason <jason@fulguros>
2023-03-13 13:56:05 -0500

    fix: addHamburgerAdditionN methods updated to match requirments

--- BillsBurgersExercise/src/Hamburger.java
+++ BillsBurgersExercise/src/Hamburger.java
@@ -17,12 +17,12 @@ public class Hamburger {
     }
 
     /**
-     * Allows adding an ingredient to the meal
+     * Allows adding an ingredient to the hamburger
      *
-     * @param item - the ingredient to add
-     * @param itemPrice - the cost of the ingredient
+     * @param item - the first ingredient to add
+     * @param itemPrice - the cost of the first ingredient
      */
-    public void addItem(String item, double itemPrice) {
+    public void addHamburgerAddition1(String item, double itemPrice) {
 	addition1Name = item;
 	addition1Price = itemPrice;
     }
@@ -30,15 +30,10 @@ public class Hamburger {
     /**
      * Allows adding an ingredient to the meal
      *
-     * @param item1 - the first ingredient to add
-     * @param item1Price - the cost of the first ingredient
      * @param item2 - the second ingredient to add
      * @param item2Price - the cost of the second ingredient
      */
-    public void addItem(String item1, double item1Price,
-			String item2, double item2Price) {
-	addition1Name = item1;
-	addition1Price = item1Price;
+    public void addHamburgerAddition(String item2, double item2Price) {
 	addition2Name = item2;
 	addition2Price = item2Price;
     }
@@ -46,20 +41,11 @@ public class Hamburger {
     /**
      * Allows adding an ingredient to the meal
      *
-     * @param item1 - the first ingredient to add
-     * @param item1Price - the cost of the first ingredient
-     * @param item2 - the second ingredient to add
-     * @param item2Price - the cost of the second ingredient
      * @param item3 - the third ingredient to add
      * @param item3Price - the cost of the third ingredient
      */
-    public void addItem(String item1, double item1Price,
-			String item2, double item2Price,
-			String item3, double item3Price) {
-	addition1Name = item1;
-	addition1Price = item1Price;
-	addition2Name = item2;
-	addition2Price = item2Price;
+    public void addHamburgerAddition(String item3, double item3Price) {
+
 	addition3Name = item3;
 	addition3Price = item3Price;
     }
@@ -67,27 +53,14 @@ public class Hamburger {
     /**
      * Allows adding an ingredient to the meal
      *
-     * @param item1 - the first ingredient to add
-     * @param item1Price - the cost of the first ingredient
-     * @param item2 - the second ingredient to add
-     * @param item2Price - the cost of the second ingredient
-     * @param item3 - the third ingredient to add
-     * @param item3Price - the cost of the third ingredient
      * @param item4 - the fourth ingredient to add
      * @param item4Price - the cost of the fourth ingredient
      */
-    public void addItem(String item1, double item1Price,
-			String item2, double item2Price,
-			String item3, double item3Price,
-			String item4, double item4Price) {
-	addition1Name = item1;
-	addition1Price = item1Price;
-	addition2Name = item2;
-	addition2Price = item2Price;
-	addition3Name = item3;
-	addition3Price = item3Price;
+    public void addHamburgerAddition4(String item4, double item4Price) {
 	addition4Name = item4;
 	addition4Price = item4Price;
     }
+
+
     
 }
