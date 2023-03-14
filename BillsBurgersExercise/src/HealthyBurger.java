d2658f5984975e3d3fa9f58668899e8c0421f020
jason <jason@fulguros>
2023-03-14 18:44:06 -0500

    Initial implementation based on requirements

--- BillsBurgersExercise/src/HealthyBurger.java
+++ BillsBurgersExercise/src/HealthyBurger.java
@@ -1,4 +1,4 @@
-public class HealthyBurger {
+public class HealthyBurger extends Hamburger {
 
     private String healthyExtra1Name, healthyExtra2Name;
     private double healthyExtra1Price, healthyExtra2Price;
@@ -7,5 +7,31 @@ public class HealthyBurger {
 	super("Healthy", meat, "Brown rye roll", price);
     }
 
-    
+    /**
+     * Adds a healthy ingredient to the already healthy burger.
+     *
+     * @param name - Name of the ingredient
+     * @param price - Cost of the ingredient
+     */
+    public void addHealthyAddition1 (String name, double price) {
+	healythExtra1Name = name;
+	healythExtra1Price = price;
+    }
+
+    /**
+     * Adds a healthy ingredient to the already healthy burger.
+     *
+     * @param name - Name of the ingredient
+     * @param price - Cost of the ingredient
+     */
+    public void addHealthyAddition2 (String name, double price) {
+	healythExtra2Name = name;
+	healythExtra2Price = price;
+    }
+
+    @Override
+    public double itemizeHamburger () {
+	return super.price + healythExtra1Price + healthyExtra2Price;
+    }
+
 }
