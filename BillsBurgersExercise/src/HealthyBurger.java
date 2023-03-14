57fc1d9e590695988bd36d26fb0fdbbd91eaa51a
jason <jason@fulguros>
2023-03-14 18:16:20 -0500

    Instance variables and constructors created

--- BillsBurgersExercise/src/HealthyBurger.java
+++ BillsBurgersExercise/src/HealthyBurger.java
@@ -1,1 +1,11 @@
-public class HealthyBurger {}
+public class HealthyBurger {
+
+    private String healthyExtra1Name, healthyExtra2Name;
+    private double healthyExtra1Price, healthyExtra2Price;
+
+    public HealthyBurger (String meat, double price) {
+	super("Healthy", meat, "Brown rye roll", price);
+    }
+
+    
+}
