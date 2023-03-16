69b311a154a7dfd48e94ab70c82bfd1ba9877af1
jason <jason@fulguros>
2023-03-16 13:47:05 -0500

    Modified to accept variable array length from user at command line

--- ArrayChallenge/src/dev/me/ArraySorting.java
+++ ArrayChallenge/src/dev/me/ArraySorting.java
@@ -6,18 +6,36 @@ import javax.swing.*;
 
 public class ArraySorting {
 
-    private static int aLen = 10;
+    private int aLen;
 
+    public ArraySorting(int arrayLength) {
+	aLen = arrayLength;
+    }
+
+    public int getLength() {
+	return aLen;
+    }
+    
     public static void main (String... args) {
 
-	int [] newArray = generateRandomArrays(aLen);
+	int len = 5;
+	if ( args.length > 0 ) {
+	    len = Integer.parseInt(args[0]);
+	}
+
+	System.out.println("Using an array length of " + len);
+	
+	ArraySorting as = new ArraySorting(len);
+					   
+	int [] newArray = generateRandomArrays(as.getLength());
+	
 	System.out.println("New: " + Arrays.toString(newArray));
 
-	int [] copied = Arrays.copyOf(newArray, aLen);
+	int [] copied = Arrays.copyOf(newArray, as.getLength());
 	Arrays.sort(copied);
 	System.out.println("Sorted: " + Arrays.toString(copied));
 
-	int [] reversed = reverseArray(copied);
+	int [] reversed = reverseArray(copied, as.getLength());
 	System.out.println("Reversed: " + Arrays.toString(reversed));
 	
 	JOptionPane.showMessageDialog(null,
@@ -52,10 +70,10 @@ public class ArraySorting {
      * @param sorted - An already sorted array of Integers
      * @return An array of integers of the same length of the input
      */
-    private static int [] reverseArray (int [] sorted) {
+    private static int [] reverseArray (int [] sorted, int length) {
 
-	int [] reversed = new int [aLen];
-	int idx = (aLen - 1);
+	int [] reversed = new int [length];
+	int idx = length - 1;
 	for (int num : sorted) {
 	    reversed[idx] = num;
 	    idx--;
