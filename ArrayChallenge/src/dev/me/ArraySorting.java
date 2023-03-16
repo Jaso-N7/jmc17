a7fefa8fded3ed40781a640f26512956183f9363
jason <jason@fulguros>
2023-03-16 13:38:31 -0500

    Completed Array Challenge

---
+++ ArrayChallenge/src/dev/me/ArraySorting.java
@@ -0,0 +1,66 @@
+package dev.me;
+
+import java.util.Random;
+import java.util.Arrays;
+import javax.swing.*;
+
+public class ArraySorting {
+
+    private static int aLen = 10;
+
+    public static void main (String... args) {
+
+	int [] newArray = generateRandomArrays(aLen);
+	System.out.println("New: " + Arrays.toString(newArray));
+
+	int [] copied = Arrays.copyOf(newArray, aLen);
+	Arrays.sort(copied);
+	System.out.println("Sorted: " + Arrays.toString(copied));
+
+	int [] reversed = reverseArray(copied);
+	System.out.println("Reversed: " + Arrays.toString(reversed));
+	
+	JOptionPane.showMessageDialog(null,
+				      "New: " + Arrays.toString(newArray) +
+				      "\nSorted: " + Arrays.toString(copied) +
+				      "\nReversed: " + Arrays.toString(reversed),
+				      "Array", 0);
+	
+	
+	System.exit(0);
+    }
+
+    /**
+     * Generates an array of random integers from 0 - 100
+     *
+     */
+    private static int [] generateRandomArrays (int len) {
+
+	Random r = new Random();
+
+	int [] trArr = new int [len];
+	for (int i = 0; i < len; i++) {
+	    trArr[i] = r.nextInt(101);
+	}
+
+	return trArr;
+    }
+
+    /**
+     * Reverses a sorted array
+     *
+     * @param sorted - An already sorted array of Integers
+     * @return An array of integers of the same length of the input
+     */
+    private static int [] reverseArray (int [] sorted) {
+
+	int [] reversed = new int [aLen];
+	int idx = (aLen - 1);
+	for (int num : sorted) {
+	    reversed[idx] = num;
+	    idx--;
+	}
+
+	return reversed;
+    }
+}
