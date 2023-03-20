00755ad9f73d168d10ffa17ef4720fbcf2bbf29c
jason <jason@fulguros>
2023-03-20 18:33:23 -0500

    Improved version that takes from command line when arguments are entered, or from prompt if called without arguments

--- MinElementChallenge/src/dev/me/MinimumElement.java
+++ MinElementChallenge/src/dev/me/MinimumElement.java
@@ -7,33 +7,35 @@ public class MinimumElement {
     public static void main (String[] elements) {
 
 	if (elements.length == 0) {
-	    System.out.println("Usage: java [-cp ...] MinimumElement n,n1,n2,...");
-	    System.out.println("Where N represents any natural number.\nNote there are no spaces between the commas and numbers.");
-	    System.exit(-1);
-	}
-	
-	int [] integers = readIntegers(elements[0].split(","));
-	System.out.println(Arrays.toString(integers));
+	    
+	    Scanner s = new Scanner(System.in);
+	    System.out.println("Enter a list of comma-separated numbers: ");
 
-	int minimumInt = findMin( integers );
-	System.out.println("The minimum integer is = " + minimumInt);
+	    displayResults( readIntegers(s.nextLine().split(",")) );
+	    
+
+	} else {
+	    displayResults( readIntegers(elements[0].split(",")) );
+	}
+
 	System.exit(0);
+	
     }
 
     /**
      * @return An array of numbers entered by the user
      */
-    public static int [] readIntegers(String... ints) {
+    private static int [] readIntegers(String... ints) {
 
 	int [] numbers = new int[ints.length];
 	for (int i = 0; i < ints.length; i++) {
-	    numbers[i] = Integer.parseInt(ints[i]);
+	    numbers[i] = Integer.parseInt(ints[i].trim());
 	}
 	    
 	return numbers;
     }
 
-    public static int findMin(int [] ints) {
+    private static int findMin(int [] ints) {
 
 	int min = ints[0];
 
@@ -44,5 +46,13 @@ public class MinimumElement {
 	    }
 	}
 	return min;
+    }
+
+    private static void displayResults(int [] integers) {
+
+	System.out.println(Arrays.toString(integers));
+
+	int minimumInt = findMin( integers );
+	System.out.println("The minimum integer is = " + minimumInt);
     }
 }
