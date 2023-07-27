8b6ce9defe9b0435732b62dc6ef22050eb3d703b
Jason Robinson <jasonsrobinson@protonmail.com>
2023-07-26 21:58:00 -0500

    Completed 179 Inner Classes Challenge

--- Section13/NestedClasses/src/learn/s13/burger/Meal.java
+++ Section13/NestedClasses/src/learn/s13/burger/Meal.java
@@ -1,9 +1,13 @@
 package learn.s13.burger;
 
+import java.util.*;
+import java.lang.Enum;
+
 public class Meal {
 
     private double base = 5.0, conversionRate;
-    private Item burger, drink, side;
+    private Burger burger;
+    private Item drink, side;
 
     public Meal () {
 	this(1);
@@ -11,15 +15,19 @@ public class Meal {
     
     public Meal (double conversionRate) {
 	this.conversionRate = conversionRate;
-	burger = new Item("regular", "burger");
+	burger = new Burger();
 	drink = new Item("coke", "drink", 1.5);
 	System.out.println(drink.name);
 	side = new Item("fries", "side", 2.0);
     }
 
     public double getTotal() {
-	double total = burger.price + drink.price + side.price;
+	double total = burger.price() + drink.price + side.price;
 	return Item.getPrice(total, conversionRate);
+    }
+
+    public void includeToppings (String... toppings) {
+	burger.addTopping(toppings);
     }
 
     @Override
@@ -55,4 +63,82 @@ public class Meal {
 	}
     }
 
+    private class Burger extends Item {
+
+	private List< Item > toppings = new LinkedList<>();
+
+	// Enums are implicitly static when used within Inner Classes
+	private enum Topping {
+	    CHEESE, KETCHUP, LETTUCE,
+	    PICKLES, ONIONS, TOMATO, PEPPERS;
+
+	    /**
+	     * Based on the topping selected, return its price
+	     *
+	     * @param {string} topping The chosen topping
+	     * @return The price of the topping
+	     */
+	    public double pricing () {
+
+		return switch(this) {
+		case CHEESE -> 0.50;
+		case KETCHUP -> 0.0;
+		case LETTUCE -> 0.08;
+		case PICKLES -> 0.07;
+		case ONIONS -> 0.05;
+		case TOMATO -> 0.45;
+		case PEPPERS -> 0.35;
+		default -> 0.0;
+		};
+	    }
+	}
+
+	public Burger () {
+	    this("regular", 10.00);
+	}
+	
+	public Burger (String name, double price) {
+	    super(name, "burger", price);
+	}
+
+	public void addTopping (String... toppingsToAdd) {
+
+	    for (String topping : toppingsToAdd) {
+
+		try {
+		    Topping selectedTopping = Topping.valueOf(topping.toUpperCase());
+		    toppings.add(new Item(selectedTopping.name(),
+					  "TOPPING",
+					  selectedTopping.pricing()));
+		} catch (IllegalArgumentException iae) {
+		    System.out.println(topping + " isn't on the menu");
+		}
+
+	    }
+	}
+
+	public double price () {
+
+	    double cost = super.price;
+
+	    for (var topping : toppings) {
+		cost += topping.price;
+	    }
+	    
+	    return cost;
+	}
+
+	@Override
+	public String toString () {
+
+	    StringBuilder burgerString = new StringBuilder("Burger:\n");
+
+	    for (Item ingredient : toppings) {
+		burgerString.append(ingredient + "\n");
+	    }
+
+	    return burgerString.toString();
+		
+	}
+    }
 }
--- Section13/NestedClasses/src/learn/s13/burger/Store.java
+++ Section13/NestedClasses/src/learn/s13/burger/Store.java
@@ -10,6 +10,11 @@ public class Store {
 	// At current 1 AUD = 103.72 JMD
 	Meal JMRegularMeal = new Meal(103.718);
 	System.out.println(JMRegularMeal);
+
+	// Regular Burger with toppings
+	Meal burgerCombo = new Meal();
+	burgerCombo.includeToppings("KETCHUP", "ONIONS", "TOMATO", "PICKLES", "PEPPERS");
+	System.out.println(burgerCombo);
     }
 
 }
