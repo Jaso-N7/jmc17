744c6688135813e345a7a5876343edbc9815fc21
Jason Robinson <jasonsrobinson@protonmail.com>
2023-07-28 16:46:22 -0500

    Created local class with calculated fields and creating a list of employees using the local class

--- Section13/LocalClassChallenge/src/dev/s13/Main.java
+++ Section13/LocalClassChallenge/src/dev/s13/Main.java
@@ -10,13 +10,42 @@ public class Main {
 
     public static void main (String [] args) {
 
+	// Setup a list of Employees with various names and hire dates
 	EmployeeModel em = new EmployeeModel();
-	System.out.println(em);
+	// confirm that em was successfully generated
+	// System.out.println(em);
 
 	for(var employee : em.viewEmployees()) {
 	    System.out.println(employee);
-	}
+	}	
+    }
+
+    /**
+     * Display the tenure of each employee
+     *
+     * @param employees A list of all employees
+     */
+    public static void employeeTenure (List<Employee> employees) {
+
+	class Tenure {
+
+	    private String fullName;
+	    private int yearsWorked;
+	    private Employee employee;
+
+	    public Tenure (Employee e) {
+		employee = e;
+		fullName = e.firstName() + " " + e.lastName();
+		yearsWorked = LocalDate.now().getYear() - e.hireDate().getYear();
+	    }
+
+	} // end of Local Class Tenure
+
+	List<Tenure> years = new LinkedList<>();
 	
+	for (var e : employees) {
+	    years.add( new Tenure(e) );
+	}
     }
 }
 
