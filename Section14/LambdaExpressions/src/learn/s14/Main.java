3c04905bd97fd597de89f992ccf1c7844d007ca2
Jason Robinson <jasonsrobinson@protonmail.com>
2023-07-30 15:55:38 -0500

    Lesson 187. Learning functional interfaces, Consumer and Predicates

--- Section14/LambdaExpressions/src/learn/s14/Main.java
+++ Section14/LambdaExpressions/src/learn/s14/Main.java
@@ -2,6 +2,7 @@ package learn.s14;
 
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.function.*;
 
 public class Main {
@@ -10,15 +11,18 @@ public class Main {
 
 	List< String > list =
 	    new LinkedList<>(List.of("alpha", "bravo", "charlie", "delta"));
-	
+
+	System.out.println("Printing a List<String> with Enhanced For: ");
 	for (String s : list) {
 	    System.out.println(s);
 	}
 
 	System.out.println("-".repeat(7));
+	System.out.println("Printing a List<String> with .forEach: ");
 	list.forEach((s) -> System.out.println(s));
 
 	System.out.println("-".repeat(7));
+	System.out.println("Using .forEach with a final locally scoped variable:");
 	String prefix = "nato"; // an effectively final local scoped variable
 	list.forEach(s -> {
 		char first = s.toUpperCase().charAt(0);
@@ -39,6 +43,38 @@ public class Main {
 	var result3 = calculator((var a, var b) ->
 				 a.toUpperCase() + " " + b.toUpperCase(),
 				 "Ralph", "Kramden");
+
+	System.out.println("Using BiConsumer:");
+	var coords = Arrays.asList(
+				   new double[]{47.2160, -97.2348},
+				   new double[]{29.1566, -89.2495},
+				   new double[]{35.1556, -90.0659});
+
+	coords.forEach(coord -> System.out.println(Arrays.toString(coord)));
+
+	BiConsumer<Double, Double> points = (lat, lng) ->
+	    System.out.format("[lat:%.3f lon:%.3f]%n", lat, lng);
+	double[] firstPoint = coords.get(0);
+	processPoint(firstPoint[0], firstPoint[1], points);
+
+	System.out.println("-".repeat(7));
+	coords.forEach(coord -> processPoint(coord[0], coord[1], points));
+	// Alternatively, using lambda directly in the method call
+	coords.forEach(coord ->
+		       processPoint(coord[0], coord[1],
+				    (lat, lng) ->
+				    System.out.format("[lat:%.3f lon:%.3f]%n", lat, lng)));
+
+	System.out.println("--- Using Predicates ---");
+	list.removeIf(s -> s.equalsIgnoreCase("bravo"));
+	list.forEach(s -> System.out.println(s));
+
+	list.addAll(List.of("echo", "easy", "earnest"));
+	list.forEach(s -> System.out.println(s));
+
+	System.out.println("-".repeat(7));
+	list.removeIf(s -> s.startsWith("ea"));
+	list.forEach(s -> System.out.println(s));
 	
 	System.out.println();
 	System.exit(0);
@@ -48,11 +84,16 @@ public class Main {
      * Conduct different operations on different types of data
      *
      */
-    public static <T> T calculator(Operation<T> function,
+    public static <T> T calculator(BinaryOperator<T> function,
 				   T value1,
 				   T value2) {
-	T result = function.operate(value1, value2);
+	T result = function.apply(value1, value2);
 	System.out.println("Result of operation: " + result);
 	return result;
+    }
+
+    public static <T> void processPoint(T point1, T point2,
+					BiConsumer<T, T> consumer){
+	consumer.accept(point1, point2);
     }
 }
